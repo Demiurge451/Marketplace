095e073b726b0531e331a8f0380486b492bf6225
Wasilewski6860 <trollivanovich686)@gmail.com>
2023-10-27 09:15:40 +0300

    Order changed

--- src/main/java/com/example/mywildberries/model/Order.java
+++ src/main/java/com/example/mywildberries/model/Order.java
@@ -4,6 +4,8 @@ package com.example.mywildberries.model;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -12,6 +14,7 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -28,7 +31,14 @@ public class Order {
     @Column
     int weight;
 
-    public Order() throws IOException {
+    @OneToMany(mappedBy = "order_owner")
+    List<Item> items;
 
+    public Order() throws IOException {
+        RestTemplate template = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
+        Map<String, String> vars = Collections.singletonMap("hotel", "42");
+        String result = restTemplate.getForObject(
+                "https://example.com/hotels/{hotel}/rooms/{hotel}", String.class, vars);
+        Person person = restTemplate.getForObject("https://example.com/people/{id}", Person.class, 42);
     }
 }
